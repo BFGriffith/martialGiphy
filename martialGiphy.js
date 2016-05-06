32d58e0554f7c2aee73db696a8e0e044496afcbb
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-05-06 08:31:05 -0400

    got starting and additional buttons working, added ShadowsIntoLight GoogleFont and some minimal styles

--- martialGiphy.js
+++ martialGiphy.js
@@ -1,6 +1,38 @@
-(function (window) {
+(function(window) {
   'use strict';
 
-  
+  /* initial GIFbuttonArray */
+  var martialGIFs = ['Toshiro Mifune', 'Bruce Lee', 'Jet Li', 'Tony Jaa', 'Michelle Yeoh', 'Zhang Ziyi', 'Shaw Brothers'];
+
+  function renderButtons() {
+    //prevents repeat buttons
+    $('#buttonsView').empty();
+    for (var i = 0; i < martialGIFs.length; i++) {
+      var a = $('<button>') //generate-button
+      a.addClass('martialGIF'); //add a class
+      a.attr('data-name', martialGIFs[i]); //add a data-attribute
+      a.text(martialGIFs[i]); //provide initial button text
+      $('#buttonsView').append(a); //add the button to HTML
+    }
+  }
+
+  $('#addMartialGIF').on('click', function() {
+
+    // This line of code will grab the input from the textbox
+    var martialGIF = $('#martialGIF_input').val().trim();
+
+    // The movie from the textbox is then added to our array
+    martialGIFs.push(martialGIF);
+
+    renderButtons();
+
+    //so that users can hit "enter" instead of clicking on the button and it won't move to the next page
+    return false;
+  })
+
+  // $(document).on('click', '.martialGIF', displayMartialGIF);
+
+
+  renderButtons();
 
 })(window);
