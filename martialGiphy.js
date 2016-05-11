894e3d5ce2fe4780e5c5d960ecc29fa7e6860c69
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-05-11 11:11:40 -0400

    adjusted to add clickable GIF-animation toggle

--- martialGiphy.js
+++ martialGiphy.js
@@ -8,7 +8,7 @@
   function displayMartialGIF() {
 
     var martialGIF = $(this).attr('data-name');
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + martialGIF + "&api_key=dc6zaTOxFJmzC";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + martialGIF + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     // AJAX call
     $.ajax({
@@ -16,25 +16,30 @@
       method: 'GET'
     }).done(function(response) {
       console.log(response);
+      var GiphyArray = response.data;
       // new <div>
-      var martialGIFdiv = $('<div class="martialGiphy">');
       var g;
-      for (g = 0; g < response.data.length; g++) {
+      for (g = 0; g < GiphyArray.length; g++) {
+        var martialGIFdiv = $('<div class="martialGiphy">');
       // create GIF
-      var image = $('<img>').attr("src", response.data[g].images.downsized_large.url);
-      // appends GIF
-      martialGIFdiv.append(image);
+      var GIFimg = $('<img>');
+      GIFimg.attr("src", GiphyArray[g].images.fixed_width_still.url);
+      GIFimg.attr("data-still", GiphyArray[g].images.fixed_width_still.url);
+      GIFimg.attr("data-animate", GiphyArray[g].images.fixed_width.url);
+      GIFimg.attr("data-state", "still");
+      GIFimg.addClass("GIFimage");
+      martialGIFdiv.append(GIFimg);
+      $('#martialGiphyView').prepend(martialGIFdiv);
+
       };
       // retrieves rating data
       var rating = response.data[0].rating;
 
-      // Creates an element to have the rating displayed
-      var pRating = $('<p>').text("Rating= " + rating);
+      // rating element creation
+      var pRating = $('<p>').text("Rating = " + rating);
 
       // display rating
-      martialGIFdiv.append(pRating);
-
-      $('#martialGiphyView').prepend(martialGIFdiv);
+      martialGIFdiv.prepend(pRating);
     });
 
   }
@@ -58,14 +63,27 @@
     //martial-arts query from the textbox is added to the array
     martialGIFs.push(martialGIF);
 
-    renderButtons();
-
     //so that users can hit "enter" instead of clicking on the button and it won't move to the next page
     return false;
   });
 
-  $(document).on('click', '.martialGIF', displayMartialGIF);
+  function animateGIF() {
+  		var state = $(this).attr("data-state");
+  		//animation toggle
+  		if (state == 'still') {
+  	        $(this).attr("src", $(this).data('animate'));
+  	        $(this).attr('data-state', 'animate');
+  	    }
+  	    if (state == 'animate') {
+  	        $(this).attr("src", $(this).data('still'));
+  	        $(this).attr('data-state', 'still');
+  	    }
+  	}
+  	//run functions
+  	$('#martialGiphyView').on('click','.GIFimage', animateGIF);
+  	$(document).on('click','.martialGIF', displayMartialGIF);
+  	$(document).on('click','#martialGIF_input', renderButtons);
 
-  renderButtons();
+renderButtons();
 
 })(window);
